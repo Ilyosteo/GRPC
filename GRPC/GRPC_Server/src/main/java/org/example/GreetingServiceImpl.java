c82781f4e7b5174277f7affd57897c8415f5d7bb
Ilyosteo <admking1@mail.ru>
2023-03-27 01:41:56 +0600

    gRPC

---
+++ GRPC/GRPC_Server/src/main/java/org/example/GreetingServiceImpl.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import com.example.grpc.GreetingServiceGrpc;
+import com.example.grpc.GreetingServiceOuterClass;
+import io.grpc.stub.StreamObserver;
+
+public class GreetingServiceImpl extends GreetingServiceGrpc.GreetingServiceImplBase {
+
+    @Override
+    public void greeting(GreetingServiceOuterClass.HelloRequest request,
+                         StreamObserver<GreetingServiceOuterClass.HelloResponse> responseObserver){
+
+        for(int i = 0; i < 10000; i++){
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            GreetingServiceOuterClass.HelloResponse response = GreetingServiceOuterClass
+                    .HelloResponse.newBuilder().setGreeting("Hello from service, Mr."+ request.getName()).build();
+
+            responseObserver.onNext(response);
+        }
+
+        responseObserver.onCompleted();
+
+
+    }
+}
